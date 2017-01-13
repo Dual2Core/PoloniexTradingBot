631f5b5438e202f304bfbff73630465a2f20d537
GarrettFleischer <garrett.fleischer@oit.edu>
2017-01-12 18:54:09 -0800

    Added ability to customize currencies in config file.

--- tradingbot.py
+++ tradingbot.py
@@ -31,17 +31,52 @@ def load_config():
 
     currency_pairs = cfg['CURRENCY']['currency_pairs'].split(',')
 
+    main_percent = 'main_percent'
+    alt_percent = 'alt_percent'
+    min_buy_profit = 'min_buy_profit'
+    min_sell_profit = 'min_sell_profit'
+    new_currency_threshold = 'new_currency_threshold'
+    new_order_threshold = 'new_order_threshold'
+    min_main = 'min_main'
+    min_alt = 'min_alt'
+    trading_history = 'trading_history'
+
+    dft_tc = TradeCurrency(currency_pair='',
+                           main_percent=float(cfg['BOT'][main_percent]) / 100.0,
+                           alt_percent=float(cfg['BOT'][alt_percent]) / 100.0,
+                           min_buy_profit=float(cfg['BOT'][min_buy_profit]) / 100.0,
+                           min_sell_profit=float(cfg['BOT'][min_sell_profit]) / 100.0,
+                           new_currency_threshold=float(cfg['BOT'][new_currency_threshold]),
+                           new_order_threshold=float(cfg['BOT'][new_order_threshold]) / 100.0,
+                           min_main=float(cfg['BOT'][min_main]),
+                           min_alt=float(cfg['BOT'][min_alt]),
+                           trading_history_in_minutes=int(cfg['BOT'][trading_history]))
+
     for pair in currency_pairs:
-        trade_items.append(TradeCurrency(currency_pair=pair,
-                                         main_percent=float(cfg['BOT']['main_percent']) / 100.0,
-                                         alt_percent=float(cfg['BOT']['alt_percent']) / 100.0,
-                                         min_buy_profit=float(cfg['BOT']['min_buy_profit']) / 100.0,
-                                         min_sell_profit=float(cfg['BOT']['min_sell_profit']) / 100.0,
-                                         new_currency_threshold=float(cfg['BOT']['new_currency_threshold']),
-                                         new_order_threshold=float(cfg['BOT']['new_order_threshold']) / 100.0,
-                                         min_main=float(cfg['BOT']['min_main']),
-                                         min_alt=float(cfg['BOT']['min_alt']),
-                                         trading_history_in_minutes=int(cfg['BOT']['trading_history'])))
+        # initialize with defaults
+        tc = TradeCurrency(currency_pair=pair,
+                           main_percent=dft_tc.main_percent,
+                           alt_percent=dft_tc.alt_percent,
+                           min_buy_profit=dft_tc.min_buy_profit,
+                           min_sell_profit=dft_tc.min_sell_profit,
+                           new_currency_threshold=dft_tc.new_currency_threshold,
+                           new_order_threshold=dft_tc.new_order_threshold,
+                           min_main=dft_tc.min_main,
+                           min_alt=dft_tc.min_alt,
+                           trading_history_in_minutes=dft_tc.trading_history_in_minutes)
+        # custom values
+        if pair in cfg:
+            tc.main_percent = cfg[pair][main_percent] if main_percent in cfg[pair] else tc.main_percent
+            tc.alt_percent = cfg[pair][alt_percent] if alt_percent in cfg[pair] else tc.alt_percent
+            tc.min_buy_profit = cfg[pair][min_buy_profit] if min_buy_profit in cfg[pair] else tc.min_buy_profit
+            tc.min_sell_profit = cfg[pair][min_sell_profit] if min_sell_profit in cfg[pair] else tc.min_sell_profit
+            tc.new_currency_threshold = cfg[pair][new_currency_threshold] if new_currency_threshold in cfg[pair] else tc.new_currency_threshold
+            tc.new_order_threshold = cfg[pair][new_order_threshold] if new_order_threshold in cfg[pair] else tc.new_order_threshold
+            tc.min_main = cfg[pair][min_main] if min_main in cfg[pair] else tc.min_main
+            tc.min_alt = cfg[pair][min_alt] if min_alt in cfg[pair] else tc.min_alt
+            tc.trading_history_in_minutes = cfg[pair][trading_history] if trading_history in cfg[pair] else tc.trading_history_in_minutes
+
+        trade_items.append(tc)
 
 
 def update_loop(algorithm):
